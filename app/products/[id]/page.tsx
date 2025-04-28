2bf4c521470b283827d5284ecffe14d6abf9991f
furioustj <furioustejash1@gmail.com>
2025-04-28 20:46:39 +0530

    params

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,14 +1,22 @@
 import { ProductDetail } from "@/components/product-detail";
 import { stripe } from "@/lib/stripe";
+import { Metadata } from "next";
 
-export default async function ProductPage({
-    params,
-}: {
-    params: { id: string };
-}) {
-    const product = await stripe.products.retrieve(params.id, {
-        expand: ["default_price"],
-    });
+// Optionally define metadata if needed
+export const metadata: Metadata = {
+  title: "Product Detail",
+};
 
-    return <ProductDetail product = {product} />;
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const product = await stripe.products.retrieve(params.id, {
+    expand: ["default_price"],
+  });
+
+  return <ProductDetail product={product} />;
 }
